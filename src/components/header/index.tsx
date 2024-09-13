b6f6aac6b3c71aea1df1823ce3dfbebfc6390f8e
Alex Nascimento <alexjose68@hotmail.com>
2024-09-12 23:21:49 -0300

    feat: instalei a biblioteca react-icons para utilizar ícones de login e lochout e montei a parte estática do Header da nossa aplicação e também o importei no layout.tsx.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,25 @@
+import Link from 'next/link';
+import { FiLogOut, FiUser } from 'react-icons/fi';
+
 export function Header() {
   return (
-    <header>
-      <h1>HEADER</h1>
+    <header className="w-full flex items-center px-2 py-4 bg-white h-20 shadow-sm">
+      <div className='w-full flex items-center justify-between max-w-7xl mx-auto'>
+        <Link href="/">
+          <h1 className='font-bold text-2xl pl-1 hover:tracking-widest duration-300'>
+            <span className='text-blue-500'>DEV</span> CONTROLE
+          </h1>
+        </Link>
+
+        <div className='flex items-baseline gap-4'>
+          <Link href="/dashboard">
+            <FiUser size={26} color='#4b5563' />
+          </Link>
+          <button>
+            <FiLogOut size={26} color='#4b5563' />
+          </button>
+        </div>
+      </div>
     </header>
   )
 }
