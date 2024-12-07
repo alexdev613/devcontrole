34a5f9e53d68354c845b1e7d1e37dd364838f619
Alex Nascimento <alexjose68@hotmail.com>
2024-12-07 16:39:54 -0300

    feat: Protegendo a rota de Dashboard para apenas usuários logado poder acessar, utilizando o getServerSession que trabalha do lado do servidor.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,18 @@
 import { Container } from "@/components/container"
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
+import { redirect } from 'next/navigation'
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  // Se não tem uma session, ou não tiver os dados do usuário numa session redireciona o usuário pra home
+  if (!session || !session.user) {
+    redirect("/")
+  }
+
+  console.log(session);
+
   return (
     <Container>
       <h1>Página dashboard</h1>
